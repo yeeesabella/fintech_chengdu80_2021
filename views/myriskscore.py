91cf1250efbd54d7a237a09dd608ea311f673abc
Isabella <54799586+yeeesabella@users.noreply.github.com>
2021-07-21 14:22:55 +0800

    new page for new uploads

---
+++ views/myriskscore.py
@@ -0,0 +1,202 @@
+# new page for new business risk score
+# Dash packages
+from app import app, User
+import numpy
+import pandas as pd
+
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import pathlib
+import networkx as nx
+from colour import Color
+from datetime import datetime
+from textwrap import dedent as d
+import json
+import pathlib
+from views import search
+import plotly.express as px
+
+
+BASE_PATH = pathlib.Path(__file__).resolve().parents[1]
+DATA_PATH = BASE_PATH.joinpath("data").resolve()
+entity_df = pd.read_csv(DATA_PATH.joinpath("huawei_data.csv"))
+watchlist_df = pd.read_csv(DATA_PATH.joinpath("my_watchlist.csv"))
+
+variables = ['Total Assets',
+'Total Liabilities',
+'Total Equity',
+'Debt to Equity (DE) Ratio',	
+'Debt Ratio',
+'Return on Assets (ROA)',
+'Return on Equity (ROE)',
+'Net Income',
+'Profit Margin',
+'Total Number Of Employees']
+entity_df['Entity ID'] = entity_df['Entity ID'].astype(int).astype(str)
+
+
+timeseries_controls = dbc.Container(dbc.Row(
+    [
+        dbc.Label("Variable of interest", style={"margin-right": "20px","verticalAlign":'middle'}),
+        dcc.Dropdown(
+            id="variable-of-interest1",
+            options=[{"label": col, "value": col} for col in variables],
+            value='Total Assets', style={"width": "20rem", "margin-right": "20px"}
+        )]))
+
+scatter_controls = dbc.Container(dbc.Row(
+    [
+        dbc.Label("X-axis", style={"margin-right": "20px","verticalAlign":'middle'}),
+        dcc.Dropdown(
+            id="x11",
+            options=[{"label": col, "value": col} for col in variables],
+            value='Total Assets', style={"width": "20rem", "margin-right": "20px"}
+        ),
+        dbc.Label("Y-axis", style={"margin-right": "20px","verticalAlign":'middle'}),
+        dcc.Dropdown(
+            id="x21",
+            options=[{"label": col, "value": col} for col in variables],
+            value='Total Liabilities', style={"width": "20rem", "margin-right": "20px"}
+        ),
+        ]))
+
+
+card_tabs=dbc.Card(
+    [
+        dbc.CardHeader(
+            dbc.Tabs(
+                [
+                    dbc.Tab(label="Financial Overview", tab_id="tab-1"),
+                    dbc.Tab(label="Risk Scores Insights", tab_id="tab-2"),
+                    dbc.Tab(label="Risks Transmission", tab_id="tab-3"),
+                    dbc.Tab(label="Comparison", tab_id="tab-4"),
+                ],
+                id="card-tabs1",
+                card=True,
+                active_tab="tab-1",
+            )
+        ),
+        dbc.CardBody(html.P(id="card-content1", className="card-text")),
+    ], style={"margin-left": "100px", "width": '1200px'}
+)
+
+risk_scores = dbc.Card(
+    dbc.CardBody(
+        [
+            dbc.Row([html.H4(["Legal risks", dbc.Badge(
+                "Low", color="success", className="ml-1",style={'margin-right': '30px'})]),
+            html.H4(["Operation risks", dbc.Badge(
+                "Low",  color="success", className="ml-1",style={'margin-right': '30px'})]),
+            html.H4(["Loan risks", dbc.Badge(
+                "Low",  color="success", className="ml-1",style={'margin-right': '30px'})]),
+            html.H4(["Other risks", dbc.Badge(
+                "Medium",  color="warning", className="ml-1",style={'margin-right': '30px'})])],justify='between'
+                ),
+            dbc.Badge("✓ Verified data secured on distributed ledger", color='light',style={'textAlign': 'left'})],
+        style={'textAlign': 'left','margin-left':'20px'}
+    ),style={'margin-left':'7%','width':'1200px'}
+)
+
+@ app.callback(
+    Output("card-content2", "children"), [Input("card-tabs2", "active_tab")]
+)
+def tab_content(active_tab):
+    if active_tab == 'tab-1':
+        return dbc.Container([
+            html.H4("Overview of Trends & Forecasts"),
+            timeseries_controls,
+            dcc.Graph(id='time-series-plot-output1',
+                  style={'width': '1000px'}),
+            html.H4("Relationship between Variables"),
+            scatter_controls,
+            dcc.Graph(id='scatter-plot-output1',
+                  style={'width': '1000px'})
+        ])
+    elif active_tab == 'tab-2':
+        return dbc.Container([html.H3("Insights into FinNUS Risk Scores"),
+                                html.P("The company is exposed to various external factors that have contributed to its operational, legal and macro risk. The primary factors that contribute to its xx risk are shown below. The explainability results are result of our proprietory algorithm which is based blockchain-protected data."),
+                                html.H3(
+                                    "Factors influencing the risk classification"),
+                                # html.H4("Global SHAP Plot"),
+                                # html.P(""),
+                                # html.Img(src='/assets/global_shap.png', height="500px"),
+                                html.P(),
+                                html.H4("Local SHAP Plot"),
+                                html.P("The output value (f(x)) is the classification class for the observation."),
+                                html.P("Red/blue: Features that push the prediction higher (to the right) are shown in red, and those pushing the prediction lower are in blue."),
+                                dbc.Badge("FinNUS Insights",style={'fontSize':'16px'}),
+                                html.P("From the above SHAP plots, the company’s high credit risk is primarily due to operating cost and tax policies."),
+                                html.Img(src='/assets/SHAP.png', height="500px"),
+                                html.P(),
+                                html.H4("LIME Plot"),
+                                html.P("The LIME plots show the different factors affecting the credit and operational risk of the company."),
+                                dbc.Badge("FinNUS Insights",style={'fontSize':'16px'}),
+                                html.P("The company has high probability for credit risk due to non-compliance with tax policies."),
+                                html.Img(src='/assets/LIME.png', height="330px"),
+                                html.P(),
+                                ])
+    elif active_tab == 'tab-3':
+        return dbc.Container([
+            html.H4("Risks Transmission"),
+            html.Img(src='/assets/ownership-nike.png', height="330px"),
+        ])
+    elif active_tab == 'tab-4':
+        return dbc.Container([
+            html.H4("Comparison with Other Risk Providers"),
+            html.Img(src='/assets/benchmark_type_2.png', height="500px")
+        ])
+    else:
+        return "This is tab {}".format(active_tab)
+
+
+@ app.callback(
+    Output("time-series-plot-output2", "figure"),
+    Input("variable-of-interest2", "value")
+)
+def plot_line_graph(variable_name):
+    df_timeseries = entity_df[(entity_df['Entity ID'] == '722691858')]
+    fig = px.bar(df_timeseries, x="Year", y=variable_name,color="is_predicted",title=f'Historical {variable_name}')
+    return fig
+
+
+@ app.callback(
+    Output("scatter-plot-output1", "figure"),
+    [Input("x11", "value"),Input("x21", "value")]
+    )
+
+def plot_scatter_graph(variable_1,variable_2):
+    df_timeseries = entity_df[(entity_df['Entity ID'] == '722691858')]
+    fig = px.scatter(df_timeseries, x=variable_1, y=variable_2,title=f'Scatter Matrix between {variable_1} and {variable_2}')
+    fig.update(layout_showlegend=False)
+    return fig
+
+layout=dbc.Container(
+                        [html.H2("FinNUS+", style={"margin-left": "100%"}),
+                        html.Hr(style={'width':'1200px','margin-left':'10%'}),
+                        dbc.Col([dbc.Row([html.H4("Entity ID",style={'margin-left':'10%'}),
+                                dbc.Badge("5721945299", pill=True,style={"fontSize": "20px", "margin-left": "13%"})]),
+                                html.P(),
+                                dbc.Row([html.H4("Company Name",style={'margin-left':'10%'}),
+                                dbc.Badge("Chengdu Inc.", pill=True,style={"fontSize": "20px", "margin-left": "6%",'margin-right':'5%'}),
+                                ]),
+                                html.P(),
+                                dbc.Col(risk_scores)
+                                ]),
+                        html.P(),
+                        card_tabs
+                        ], className="mt-4")
+
+
+@app.callback(
+    Output("example-output1", "children"), [
+        Input("add-to-watchlist-button1", "n_clicks")]
+)
+def on_button_click(n):
+    if n==0:
+        return " "
+    else:
+        return f"✓ Added to alerts!"
