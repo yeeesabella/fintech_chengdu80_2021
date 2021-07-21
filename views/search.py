d05d0a5e74ac2c073bb51d3a5165e0fbeddd20e5
Isabella <54799586+yeeesabella@users.noreply.github.com>
2021-07-21 11:21:50 +0800

    search fix social media alignment

--- views/search.py
+++ views/search.py
@@ -35,36 +35,25 @@ show_description = dbc.Card(
                 className="card-text",
             )
         ]
-    ), style={'width': '800px','margin-left':'15px'}
+    ), style={'width': '800px', 'margin-left': '15px'}
 )
 
 risk_scores = dbc.Card(
     dbc.CardBody(
-        [   
+        [
             html.H5(["Legal risks", dbc.Badge(
                 "High", color="danger", className="ml-1")]),
             html.H5(["Operation risks", dbc.Badge(
                 "Medium",  color="warning", className="ml-1")]),
             html.H5(["Loan risks", dbc.Badge(
                 "Low",  color="success", className="ml-1")]),
-            html.H5(["Other risks", dbc.Badge("Low",  color="success", className="ml-1")]),
-            dbc.Badge("✓ Based on data secured on distributed ledger",color='light')],
+            html.H5(["Other risks", dbc.Badge(
+                "Low",  color="success", className="ml-1")]),
+            dbc.Badge("✓ Based on data secured on distributed ledger", color='light')],
         # html.H5(view_risk_analysis)],
-        style={'textAlign':'right'}
+        style={'textAlign': 'right'}
     )
 )
-
-
-# social_media_card = dbc.Card(
-#     dbc.CardBody(
-#         [
-#             html.Img(
-#                 src='/assets/socialmedia_mockup.png',
-#                 className="logo", style={'height': '30%', 'width': '30%'}
-#             ),
-#         ],
-#         style={"width": "20"}
-#     ))
 
 company_news_card = dbc.Card(
     dbc.CardBody(
@@ -79,19 +68,25 @@ company_news_card = dbc.Card(
                     dbc.NavItem(dbc.NavLink("Ericsson warns of China retaliation following Sweden's ...",
                                             href="https://www.ft.com/content/2a596954-1206-4ce2-9dca-9c128d326768")),
                 ],
-                vertical="md",
+                className="card-text",
             )
         ]
-    ),style={'width':'800px'}
+    ), style={'height':'340px','width': '300px', 'margin-left': '10px'}
 )
 
-
-# sentiment_data = dbc.Row(children=[
-#     dbc.Col(company_news_card, width=4, style={'margin-left': '100'}),
-#     dbc.Col(social_media_card, width=4, style={'margin-left': '400'}),
-# ]
-# )
-
+show_social = dbc.Card(
+    dbc.CardBody(
+        [html.H4("Social", className="card-title"), html.A([
+            html.Img(
+                src='/assets/twitter.jpeg',
+                style={
+                    'height': '50%',
+                    'width': '50%',
+                })
+    ], href='https://twitter.com/search?q=huawei&src=typed_query')],
+        style={'textAlign': 'left'}
+    ),style={'width':'300px','margin-left': '10px'}
+)
 
 view_forecast = html.Div(
     [
@@ -123,14 +118,19 @@ row11 = html.Tr([html.Td("Registered capital"), html.Td("300")])
 row12 = html.Tr([html.Td("Paid Up Capital"), html.Td("3")])
 
 
-table_body = [html.Tbody([row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12])]
+table_body = [html.Tbody(
+    [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12])]
 
 
 layout = dbc.Container(
-    [html.Br(),
-     dbc.Badge("Entity ID: 2608434", style={"fontSize": "24px"}),
-     html.P(),
-     dbc.Badge("Company Name: Huawei", style={"fontSize": "24px"}),
+    [html.H2("Company Details", style={'textAlign': 'right'}),
+    html.Hr(style={'width': '1100px'}),
+        html.Br(),
+        dbc.Row([html.H4("Entity ID", style={'margin-left': '1%'}),
+                dbc.Badge("722691858", pill=True, style={"fontSize": "20px", "margin-left": "6%"})]),
+        html.P(),
+        dbc.Row([html.H4("Company Name", style={'margin-left': '1%'}),
+                dbc.Badge("Huawei", pill=True, style={"fontSize": "20px", "margin-left": "3%", 'margin-right': '3%'})]),
      html.P(),
      dbc.Row([show_description, risk_scores], justify='between'),
      dbc.Button(
@@ -140,21 +140,14 @@ layout = dbc.Container(
                'font-size': '16px'}
     ),
         html.P(),
-        dbc.Col([dbc.Table(table_header+table_body, bordered=True,size='md')
-                        ], align='left',style={'width':'800px'}),
+        dbc.Row([dbc.Table(table_header+table_body, bordered=True, size='md',style={'width': '800px'}),
+        dbc.Col([company_news_card,html.P(),show_social])],justify='between'),
+        # dbc.Col([dbc.Table(table_header+table_body, bordered=True, size='md')
+        #                 ], align='left', style={'width': '800px'}),
         dbc.Button("View trends & forecasts", id="view_forecast", href='/payment',
                    outline=True, color='primary', className="mr-2",
-                   style={'textAlign': 'center','width': '800px', 'margin-top': '10px','font-size': '16px'}),
-        html.P(),company_news_card,
-        html.Div([
-    html.A([
-            html.Img(
-                src='/assets/twitter.jpeg',
-                style={
-                    'height' : '20%',
-                    'width' : '20%',
-                })
-    ], href='https://twitter.com/search?q=huawei&src=typed_query')]),
+                   style={'textAlign': 'center', 'width': '790px', 'margin-top': '10px', 'font-size': '16px'}),
+        html.P(),
         html.P()
     ], style={'margin-left': '20%'}
 )
